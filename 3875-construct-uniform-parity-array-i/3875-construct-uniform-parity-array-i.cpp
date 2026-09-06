d358fd5037b5df65ece24ee3ddf6f2abd179bd3a
Ayush <tysontechnopoint@gmail.com>
2026-09-07 01:25:40 +0530

    Time: 0 ms (100%), Space: 30.1 MB (91.39%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+    }
+};
+//(l*de ke baal -  agar mixed hai to =(ek odd rakh , even me se odd wale ko subtract kar  de to odd hi ban jayega )) (isliye ansh g*y hai)
